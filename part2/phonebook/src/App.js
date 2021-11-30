2cb50a60db1fb1b306f48c5ac97fb03507bc98da
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2021-11-30 15:37:12 -0300

    Finished exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addContact = (event) => {
+    event.preventDefault();
+    const newContact = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newContact));
+    setNewName("");
+  };
+  const handleNameChange = (event) => setNewName(event.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <label htmlFor="name">
+          Name:
+          <input id="name" value={newName} onChange={handleNameChange} />
+        </label>
+        <button>add contact</button>
+      </form>
+
+      <h2>Numbers</h2>
+      {persons.map((person, i) => (
+        <p key={i}>{person.name}</p>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
