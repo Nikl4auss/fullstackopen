911852a17d7b834d3b231eb2a211739911b67fab
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2021-11-30 12:09:53 -0300

    Finished part 2b lecture

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,14 +1,47 @@
-import { useState } from "react"
-import Note from "./components/Note"
-const App = ({notes}) => {
+import { useState } from "react";
+import Note from "./components/Note";
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
   return (
     <section>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => <Note note={note} />)}
+        {notesToShow.map((note) => (
+          <Note note={note} />
+        ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default App
+export default App;
