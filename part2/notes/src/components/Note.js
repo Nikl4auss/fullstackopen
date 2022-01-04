108af4b5548134f0828eed36896b6772bc48b8f9
nikl4auss <nicolasmarinelli@protonmail.com>
2022-01-04 11:04:03 -0300

    Finished lecture 2.e

--- part2/notes/src/components/Note.js
+++ part2/notes/src/components/Note.js
@@ -4,7 +4,7 @@ const Note = ({ note, toggleImportance }) => {
   : "make important"
 
   return (
-  <li key={note.id}>
+  <li key={note.id} className="note">
     {note.content}
     <button onClick={toggleImportance}>{label}</button>
     </li>);
