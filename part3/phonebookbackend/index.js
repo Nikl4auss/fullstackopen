deab21d0780d1ad30e8627b441733829591b03a6
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-02-19 20:09:52 -0300

    Finished exercise 3.10

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -80,7 +80,7 @@ app.delete("/api/contacts/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
