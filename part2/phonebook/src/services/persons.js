1af83a49b656fda1fa6a4169793670d18ef97224
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-02-19 19:38:18 -0300

    Finished exercise 3.9

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,30 +1,30 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = "/api/contacts";
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-const add = newObject => {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response.data)
-}
+const add = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
 
 const update = (newObject, id) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
 
-const remove = id => {
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response => response)
-}
+const remove = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response);
+};
 
 export default {
-    getAll,
-    add,
-    update,
-    remove
-}
+  getAll,
+  add,
+  update,
+  remove,
+};
