fd93758cf03d3edc7176a89aecf4ea270acc9b56
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-02-27 13:38:14 -0300

    Finished lecture 3.c

--- part3/notesbackend/index.js
+++ part3/notesbackend/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { response } = require("express");
+const { response, request } = require("express");
 const cors = require("cors");
 const express = require("express");
 const Note = require("./models/note");
@@ -41,14 +41,13 @@ app.get("/api/notes", (request, response) => {
   Note.find({}).then((notes) => response.json(notes));
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
   const id = request.params.id;
   Note.findById(id)
-    .then((findedNote) => response.json(findedNote))
-    .catch((error) => {
-      response.statusMessage = "Note doesn't exist";
-      response.status(404).end();
-    });
+    .then((findedNote) =>
+      findedNote ? response.json(findedNote) : response.status(404).end()
+    )
+    .catch((error) => next(error));
 });
 
 app.post("/api/notes", (request, response) => {
@@ -63,12 +62,43 @@ app.post("/api/notes", (request, response) => {
   note.save().then((savedNote) => response.json(savedNote));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id === id);
-  console.log("Note deleted");
-  response.status(204).end();
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  if (!body.content)
+    return response.status(400).json({ error: "content missing" });
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => response.json(updatedNote))
+    .catch((error) => next(error));
 });
+
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => response.status(204).end)
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.message === "CastError")
+    return response.status(400).send({ error: "malformatted id" });
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
