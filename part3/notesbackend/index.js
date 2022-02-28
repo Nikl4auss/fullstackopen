723677f19aae4518e0d89c64894474b329127a17
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-02-28 20:40:45 -0300

    Finished lecture 3.d validation creation

--- part3/notesbackend/index.js
+++ part3/notesbackend/index.js
@@ -4,36 +4,10 @@ const cors = require("cors");
 const express = require("express");
 const Note = require("./models/note");
 const app = express();
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
 
 app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
-const genereateId = () => {
-  const maxID =
-    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
-
-  return maxID + 1;
-};
 
 app.get("/", (request, response) => {});
 
@@ -50,29 +24,31 @@ app.get("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-  if (!body.content)
-    return response.status(400).json({ error: "content missing" });
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
   });
-  note.save().then((savedNote) => response.json(savedNote));
+  note
+    .save()
+    .then((savedNote) => response.json(savedNote))
+    .catch((error) => next(error));
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
   const body = request.body;
-
-  if (!body.content)
-    return response.status(400).json({ error: "content missing" });
   const note = {
     content: body.content,
     important: body.important,
   };
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedNote) => response.json(updatedNote))
     .catch((error) => next(error));
 });
@@ -92,9 +68,11 @@ app.use(unknownEndpoint);
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
-  if (error.message === "CastError")
+  if (error.name === "CastError")
     return response.status(400).send({ error: "malformatted id" });
-
+  if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
   next(error);
 };
 
--- part3/notesbackend/models/note.js
+++ part3/notesbackend/models/note.js
@@ -10,8 +10,15 @@ mongoose
   .catch((error) => console.log("error connecting to MongoDB", error.message));
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
+  content: {
+    type: String,
+    minlength: 5,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
 });
 
