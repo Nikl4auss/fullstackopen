895739fab7a8687e099dd732531101279985c640
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-03-01 18:59:43 -0300

    Finished exercise 3.22 and part 3.d

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,20 +1,21 @@
-require("dotenv").config();
-const { response } = require("express");
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const Contact = require("./models/contact");
+/* eslint-disable no-console */
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const Contact = require('./models/contact');
+
 const app = express();
 
-morgan.token("body", (request, response) => JSON.stringify(request.body));
+morgan.token('body', (request) => JSON.stringify(request.body));
 
-app.use(express.static("build"));
+app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'),
 );
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   Contact.find({}).then((contacts) => {
     response.end(`
     <p>Phonebook has info for ${contacts.length} people</p>
@@ -23,20 +24,18 @@ app.get("/info", (request, response) => {
   });
 });
 
-app.get("/api/contacts", (request, response) => {
+app.get('/api/contacts', (request, response) => {
   Contact.find({}).then((contacts) => response.json(contacts));
 });
 
-app.get("/api/contacts/:id", (request, response, next) => {
+app.get('/api/contacts/:id', (request, response, next) => {
   Contact.findById(request.params.id)
-    .then((contact) =>
-      contact ? response.json(contact) : response.status(404).end()
-    )
+    .then((contact) => (contact ? response.json(contact) : response.status(404).end()))
     .catch((error) => next(error));
 });
 
-app.post("/api/contacts", (request, response, next) => {
-  const body = request.body;
+app.post('/api/contacts', (request, response, next) => {
+  const { body } = request;
   const { name, number } = body;
 
   const contact = new Contact({
@@ -52,8 +51,8 @@ app.post("/api/contacts", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.put("/api/contacts/:id", (request, response, next) => {
-  const body = request.body;
+app.put('/api/contacts/:id', (request, response, next) => {
+  const { body } = request;
   const { name, number } = body;
   const contact = {
     name,
@@ -63,30 +62,30 @@ app.put("/api/contacts/:id", (request, response, next) => {
   Contact.findByIdAndUpdate(request.params.id, contact, {
     new: true,
     runValidators: true,
-    context: "query",
+    context: 'query',
   })
     .then((contactUpdated) => response.json(contactUpdated))
     .catch((error) => next(error));
 });
 
-app.delete("/api/contacts/:id", (request, response, next) => {
+app.delete('/api/contacts/:id', (request, response, next) => {
   Contact.findByIdAndDelete(request.params.id)
-    .then((result) => response.status(204).end())
+    .then(() => response.status(204).end())
     .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
+  response.status(404).send({ error: 'unknown endpoint' });
 };
 
 app.use(unknownEndpoint);
 
+// eslint-disable-next-line consistent-return
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
-  if (error.name === "CastError")
-    return response.status(400).send({ error: "malformatted id" });
-  if (error.name === "ValidationError") {
+  if (error.name === 'CastError') return response.status(400).send({ error: 'malformatted id' });
+  if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
   }
 
--- part3/phonebookbackend/models/contact.js
+++ part3/phonebookbackend/models/contact.js
@@ -1,25 +1,29 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-console */
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
 const url = process.env.MONGODB_URI;
 
-console.log("Conecting to ", url);
+console.log('Conecting to ', url);
 
 mongoose
   .connect(url)
-  .then((response) => console.log("Connected to MongoDB"))
-  .catch((error) => console.log("error connecting to MongoDB", error.message));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB', error.message));
 
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: [3, "Contact name must be at least 3 characters long"],
-    required: [true, "Contact Name Required!"],
-    unique: [true, "Name must be unique"],
+    minlength: [3, 'Contact name must be at least 3 characters long'],
+    required: [true, 'Contact Name Required!'],
+    unique: [true, 'Name must be unique'],
   },
   number: {
     type: String,
-    minlength: [8, "phone number must be at least 8 characters long"],
-    required: [true, "phonenumber is required"],
+    minlength: [8, 'phone number must be at least 8 characters long'],
+    required: [true, 'phonenumber is required'],
     validate: {
       validator: (value) => /\d{2,3}-\d*/.test(value),
       message: (props) => `${props.value} it's not a valid phone number`,
@@ -37,7 +41,7 @@ const contactSchema = new mongoose.Schema({
 
 contactSchema.plugin(uniqueValidator);
 
-contactSchema.set("toJSON", {
+contactSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
@@ -45,4 +49,4 @@ contactSchema.set("toJSON", {
   },
 });
 
-module.exports = mongoose.model("Contact", contactSchema);
+module.exports = mongoose.model('Contact', contactSchema);
