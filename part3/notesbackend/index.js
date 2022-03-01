8aea601be9bfc2951f573e7174090d6f2e7cc91a
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-03-01 15:29:09 -0300

    Finished lecture 3.d

--- part3/notesbackend/index.js
+++ part3/notesbackend/index.js
@@ -1,84 +1,83 @@
-require("dotenv").config();
-const { response, request } = require("express");
-const cors = require("cors");
-const express = require("express");
-const Note = require("./models/note");
-const app = express();
+require('dotenv').config()
+const cors = require('cors')
+const express = require('express')
+const Note = require('./models/note')
+const app = express()
 
-app.use(express.static("build"));
-app.use(cors());
-app.use(express.json());
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (request, response) => {});
 
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => response.json(notes));
-});
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then((notes) => response.json(notes))
+})
 
-app.get("/api/notes/:id", (request, response, next) => {
-  const id = request.params.id;
+app.get('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
   Note.findById(id)
     .then((findedNote) =>
       findedNote ? response.json(findedNote) : response.status(404).end()
     )
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body;
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-  });
+  })
   note
     .save()
     .then((savedNote) => response.json(savedNote))
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
   const note = {
     content: body.content,
     important: body.important,
-  };
+  }
 
   Note.findByIdAndUpdate(request.params.id, note, {
     new: true,
     runValidators: true,
-    context: "query",
+    context: 'query',
   })
     .then((updatedNote) => response.json(updatedNote))
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/notes/:id", (request, response, next) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then((result) => response.status(204).end())
-    .catch((error) => next(error));
-});
+    .then(() => response.status(204).end())
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError")
-    return response.status(400).send({ error: "malformatted id" });
-  if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError')
+    return response.status(400).send({ error: 'malformatted id' })
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/notesbackend/models/note.js
+++ part3/notesbackend/models/note.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+// eslint-disable-next-line no-undef
+const url = process.env.MONGODB_URI
 
-console.log("connecting to ", url);
+console.log('connecting to ', url)
 
 mongoose
   .connect(url)
-  .then((result) => console.log("conected to MongoDB"))
-  .catch((error) => console.log("error connecting to MongoDB", error.message));
+  .then(() => console.log('conected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB', error.message))
 
 const noteSchema = new mongoose.Schema({
   content: {
@@ -20,14 +21,14 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
-});
+})
 
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+noteSchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
