8e5d53cb7c9926a0cdb1b64ed6b1fdae6e180224
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-03-22 02:06:00 -0300

    Finished exercise 4.10

--- part3/bloglist/tests/blog_api.test.js
+++ part3/bloglist/tests/blog_api.test.js
@@ -15,22 +15,45 @@ beforeEach(async () => {
     }
 })
 
-test('blogs are returned as JSON', async () => {
-    await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-}, 10000)
+describe('GET requests', () => {
+    
+    test('blogs are returned as JSON', async () => {
+        await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    }, 10000)
+    
+    test('at the start, there should be 4 blogs', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+    
+    test('when returned as JSON, blogs should have the id property', async () => {
+        const blogs = await helper.blogsInDB()
+        expect(blogs[0].id).toBeDefined()
+    })
+});
 
-test('at the start, there should be 4 blogs', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+describe('POST request', () => {
+    test('a valid blog can be added', async () => {
+        const newBlog = {
+            title: "Something something blog",
+            author: "Someone",
+            url: "https://dontclickthislink.com/",
+            likes: 10,
+        }
 
-test('when returned as JSON, blogs should have the id property', async () => {
-    const blogs = await helper.blogsInDB()
-    expect(blogs[0].id).toBeDefined()
-})
+        await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+        const blogsAtEnd = await helper.blogsInDB()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    })
+});
 afterAll(() => {
     mongoose.connection.close()
 })
